64a5559346269ccbc3215acca033a3a364268e11
Ericcost <eric.costerousse@gmail.com>
2023-03-02 16:07:23 +0100

    Add a new  Prehome

--- noesia_app/src/pages/PreHome/PreHome.jsx
+++ noesia_app/src/pages/PreHome/PreHome.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import './PreHome.scss'
 
 export default function PreHome() {
   return (
-    <div>PreHome</div>
+      
+    <div className='prehome-container'>
+      <div className="prehome">
+        <h1>Welcome to Noesia</h1>
+        <p>Blabla présentation du projet/univers by Ericu.</p>
+        <button>Découvrir Noesia</button>
+      </div>
+    </div>
+      
   )
 }
