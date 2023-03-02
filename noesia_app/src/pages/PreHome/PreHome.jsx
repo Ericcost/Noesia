17dd0e7da87827850c75044ebe920c27de59a73d
Ericcost <eric.costerousse@gmail.com>
2023-03-02 17:13:51 +0100

    PreHome's background fixed

--- noesia_app/src/pages/PreHome/PreHome.jsx
+++ noesia_app/src/pages/PreHome/PreHome.jsx
@@ -3,14 +3,14 @@ import './PreHome.scss'
 
 export default function PreHome() {
   return (
-      
-    <div className='prehome-container'>
-      <div className="prehome">
-        <h1>Welcome to Noesia</h1>
-        <p>Blabla présentation du projet/univers by Ericu.</p>
-        <button>Découvrir Noesia</button>
+    <div className='prehome-background'>
+      <div className='prehome'>
+        <div className="prehome-content">
+          <h1>Welcome to Noesia</h1>
+          <p>Blabla présentation du projet/univers by Ericu.</p>
+          <button>Découvrir Noesia</button>
+        </div>
       </div>
     </div>
-      
   )
 }
