0d4d09ddc1770989864237cb3e666ed519064745
Ericcost <eric.costerousse@gmail.com>
2023-03-02 15:16:57 +0100

    little modification

--- noesia_app/src/main.jsx
+++ noesia_app/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './style.scss'
+import './styles/style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
 )
