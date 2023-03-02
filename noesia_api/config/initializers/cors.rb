843307ea01198d27a874b21b3f59c6606fd603dc
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-02 15:19:33 +0000

    Add Rails Rack CORS middleware for API request

--- noesia_api/config/initializers/cors.rb
+++ noesia_api/config/initializers/cors.rb
@@ -5,12 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# These changes allow any site to make requests to the API,
+# to allow a single origin ➡️ origins "[url]"
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose: %w[Authorization Uid]
+  end
+end
