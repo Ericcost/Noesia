e461769f64c64952580cf920f52367f3e601234b
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-10 15:27:48 +0000

    Update Profile: Working

--- noesia_app/src/pages/Profile/Profile.jsx
+++ noesia_app/src/pages/Profile/Profile.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { useGetUser } from '../../hooks/useGetUser';
 
 export default function Profile() {
+
+  const auth_token = localStorage.getItem('Authorization_token');
+  const { isLoading, data, isError, error } = useGetUser('member-data', auth_token);
+
+  const current_user = data?.user;
+
   return (
-    <div>Profile</div>
+    <>
+      <div>{current_user?.username}</div>
+      <div>{current_user?.email}</div>
+      {isLoading && <div>Loading ...</div>}
+      {isError && <div>Une erreur s'est produite : {error.message}</div>}
+    </>
+
   )
 }
