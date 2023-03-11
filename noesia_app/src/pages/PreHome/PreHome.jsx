6de050d26b0525dca85ee8598db91eb02d1685f2
Ericcost <eric.costerousse@gmail.com>
2023-03-11 21:00:11 +0100

    add the cursor component

--- noesia_app/src/pages/PreHome/PreHome.jsx
+++ noesia_app/src/pages/PreHome/PreHome.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import{ Link } from 'react-router-dom'
 import { TypeAnimation } from 'react-type-animation';
 import preHomeBackground from '../../assets/images/prehome_background.png'
+import Cursor from '../../components/Cursor/Cursor'
 import Button from '../../components/Button/Button'
 import './PreHome.scss'
 
@@ -17,44 +18,47 @@ export default function PreHome() {
   }
   
   return (
-    <div className='prehome-background'>
-      <img src={preHomeBackground} alt="" className='prehome-background-img'/>
-      { story ? 
-        <div className="story-overlay">
-          <div className="story-content">
-            <div className='story-title'>
-              <h1>NOESIA</h1>
-            </div>
-            <div className='story-plot'>
-              <p><TypeAnimation
-                sequence={[
-                  1000,
-                  'Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue', // Types 'One'
-                  1000, 
-                  'Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot',
-                  1000,
-              
-                ]}
-                wrapper="strong"
-                cursor={true}
-                repeat={Infinity}
-                style={{ fontSize: '1em', paddingLeft: '5px' }}
-              /></p>
-            </div>
-            <div className='story-btn'>
-              <button onClick={handleStory}>Close</button>
+    <>
+      <Cursor />
+      <div className='prehome-background'>
+        <img src={preHomeBackground} alt="" className='prehome-background-img'/>
+        { story ? 
+          <div className="story-overlay">
+            <div className="story-content">
+              <div className='story-title'>
+                <h1>Noesia</h1>
+              </div>
+              <div className='story-plot'>
+                <p><TypeAnimation
+                  sequence={[
+                    1000,
+                    'Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue Intrigue', // Types 'One'
+                    1000, 
+                    'Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot Plot',
+                    1000,
+                
+                  ]}
+                  wrapper="strong"
+                  cursor={true}
+                  repeat={Infinity}
+                  style={{ fontSize: '1em', paddingLeft: '5px' }}
+                  /></p>
+              </div>
+              <div className='story-btn'>
+                <button onClick={handleStory}>Close</button>
+              </div>
             </div>
           </div>
-        </div>
-        :
-        <div className='prehome'>
-          <div className="prehome-content">
-            <Link to="/accueil"> 
-              <Button content="Découvrir Noesia"/>
-            </Link>
+          :
+          <div className='prehome'>
+            <div className="prehome-content">
+              <Link to="/accueil"> 
+                <Button content="Découvrir Noesia"/>
+              </Link>
+            </div>
           </div>
-        </div>
-      }
-    </div>
+        }
+      </div>
+    </>
   )
 }
