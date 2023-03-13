35f8770d96a1cf7042c2cb9bbd164b014774b54f
Ericcost <eric.costerousse@gmail.com>
2023-03-13 19:52:40 +0100

    New Hook to get enigmas

---
+++ noesia_app/src/hooks/enigma/useGetEnigmas.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+import { API_URL } from '../../services/API_URL';
+
+export function useGetEnigmas(endpoint) {
+  return useQuery('enigmas', async () => {
+    const response = await axios.get(`${API_URL}/${endpoint}`)
+    return response
+  })
+};
