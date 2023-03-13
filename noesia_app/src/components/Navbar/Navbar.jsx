c849e0cadffd632b4da77c765014d8fc0cc5ac82
Ericcost <eric.costerousse@gmail.com>
2023-03-13 23:32:19 +0100

    navbar reorganization

--- noesia_app/src/components/Navbar/Navbar.jsx
+++ noesia_app/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import "./Navbar.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDeleteUser } from "../../hooks/user/useDeleteUser";
-import { useNavigate } from "react-router-dom";
 import { useGetUser } from "../../hooks/user/useGetUser";
 
 const Navbar = () => {
@@ -9,17 +8,16 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   const auth_token = localStorage.getItem('Authorization_token');
-
   const { isLoading, data, isError, error } = useGetUser('member-data', auth_token);
-  const { mutate: deleteUser } = useDeleteUser('users/sign_out', auth_token);
-
-  const logged = auth_token ? true : false;
   const current_user = data?.user;
+  const { mutate: deleteUser } = useDeleteUser('users/sign_out', auth_token);
+  const logged = auth_token ? true : false;
+  
 
   const handleLogout = (e) => {
     localStorage.removeItem('Authorization_token');
     deleteUser(auth_token);
-    navigate('/accueil');
+    navigate('/');
   }
 
   return (
