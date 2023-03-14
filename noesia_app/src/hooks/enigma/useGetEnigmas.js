91753b068d972e5809a9d423413a363149d7a2cb
Ericcost <eric.costerousse@gmail.com>
2023-03-14 10:31:38 +0100

    Test to check request from map page

--- noesia_app/src/hooks/enigma/useGetEnigmas.js
+++ noesia_app/src/hooks/enigma/useGetEnigmas.js
@@ -5,6 +5,7 @@ import { API_URL } from '../../services/API_URL';
 export function useGetEnigmas(endpoint) {
   return useQuery('enigmas', async () => {
     const response = await axios.get(`${API_URL}/${endpoint}`)
+    // console.log(response.data);
     return response
   })
 };
