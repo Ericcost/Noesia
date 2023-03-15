b3bf51765bb6ce64dadecf868ec00426641b9daa
Ericcost <eric.costerousse@gmail.com>
2023-03-15 18:44:58 +0100

    Add the door failed try

--- noesia_app/src/pages/Door/Door.jsx
+++ noesia_app/src/pages/Door/Door.jsx
@@ -70,7 +70,6 @@ export default function Door() {
                 de découvertes passionnantes et de merveilles insoupçonnées.
               </p>
               <button onClick={handleSubmit}>Valider</button>
-              <ButtonLink />
             </div>
           </div>
 
