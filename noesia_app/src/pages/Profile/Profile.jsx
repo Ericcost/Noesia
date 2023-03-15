addb057f34bb3924b93511ae2840ae0aeb8af2aa
Ericcost <eric.costerousse@gmail.com>
2023-03-15 18:43:26 +0100

    Remove the html, body scss balise

--- noesia_app/src/pages/Profile/Profile.jsx
+++ noesia_app/src/pages/Profile/Profile.jsx
@@ -13,31 +13,34 @@ export default function Profile() {
   const { isLoading, data, isError, error } = useGetUser('member-data', auth_token);
 
   const current_user = data?.user;
+  console.log(data?.user)
 
   return (
     <>
       <Cursor />
-      <div className='profile-wrapper'>
-        <div className='profile-left'>
-          <div className='profile-left-top'>
-            <img className='profile-picture' src="../src/assets/images/background.jpg" />
-            <div class="profile-info">
-              <h2 class="profile-name">Nom de l'utilisateur</h2>
-              <Button content="Editer le profil" path="/profil/editer"/>
+      <div className='profile'>
+        <div className='profile-wrapper'>
+          <div className='profile-left'>
+            <div className='profile-left-top'>
+              <img className='profile-picture' src="../src/assets/images/background.jpg" />
+              <div class="profile-info">
+                <h2 class="profile-name">Nom de l'utilisateur</h2>
+                <Button content="Editer le profil" path="/profil/editer"/>
+              </div>
+            </div>
+            <div className='profile-left-bottom'>
+              <h2>Caractéristiques</h2>
+              <p>Caract</p>
+              <p>Caract</p>
+              <p>Caract</p>
+              <p>Caract</p>
+              <p>Caract</p>
+              <p>Caract</p>
             </div>
           </div>
-          <div className='profile-left-bottom'>
-            <h2>Caractéristiques</h2>
-            <p>Caract</p>
-            <p>Caract</p>
-            <p>Caract</p>
-            <p>Caract</p>
-            <p>Caract</p>
-            <p>Caract</p>
+          <div className='profile-right'>
+            <div className='profile-right-header'>Historique des parties</div>
           </div>
-        </div>
-        <div className='profile-right'>
-          <div className='profile-right-header'>Historique des parties</div>
         </div>
       </div>
     </>
