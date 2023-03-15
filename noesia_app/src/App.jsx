2448977c526a9a5e1230f80ad297db3fc1b37e8a
KevinTAZARO <kenee23@hotmail.fr>
2023-03-15 08:34:26 +0100

    remote mouse effect

--- noesia_app/src/App.jsx
+++ noesia_app/src/App.jsx
@@ -8,6 +8,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Profile from "./pages/Profile/Profile";
+import Door from "./pages/Door/Door";
 
 //Components
 import Footer from "./components/Footer/Footer";
@@ -21,6 +22,7 @@ function App() {
               <Route path="/connexion" element={<Login />} />
               <Route path="/inscription" element={<Register />} />
               <Route path="/profil/:id" element={<Profile />} />
+              <Route path="/tutoriel" element={<Door />} />
             </Routes>
         </main>
         <footer>
