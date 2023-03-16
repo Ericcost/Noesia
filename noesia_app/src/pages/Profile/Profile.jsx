2a890febdf631780d08aa814e41f7bb5e9ce1093
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-16 17:58:50 +0000

    Update profil with new features

--- noesia_app/src/pages/Profile/Profile.jsx
+++ noesia_app/src/pages/Profile/Profile.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useState } from 'react';
 
 import Cursor from '../../components/Cursor/Cursor'
 import Button from '../../components/ButtonLink/ButtonLink';
-import Sidebar from '../../components/Sidebar/Sidebar';
 
+import { useGetUserHistories } from '../../hooks/history/useGetUserHistories';
+import { useGetUserAchievements } from '../../hooks/user_achievement/useGetUserAchievements';
+import { useGetAchievements } from '../../hooks/achievement/useGetAchievements';
 import { useGetUser } from '../../hooks/user/useGetUser';
 
+import { RiSurgicalMaskLine } from 'react-icons/ri'
+import { BsGear } from 'react-icons/bs'
+import { GiSwordsPower, GiConcentrationOrb, GiChest } from 'react-icons/gi'
+import { FaFeatherAlt, FaTrophy } from 'react-icons/fa'
+import { SiAlchemy } from 'react-icons/si'
 import './Profile.scss'
 
 export default function Profile() {
@@ -14,35 +21,143 @@ export default function Profile() {
   const { isLoading, data, isError, error } = useGetUser('member-data', auth_token);
 
   const current_user = data?.user;
-  console.log(data?.user)
+
+  const { isLoading: isHistoriesLoading, data: historiesData, isError: isHistoriesError, error: historiesError, isSuccess: isHistoriesSuccess } = useGetUserHistories('histories', current_user?.id);
+  const { isLoading: isAchievementsLoading, data: AchievementsData, isError: isAchievementsError, error: AchievementsError, isSuccess: isAchievementsSuccess } = useGetAchievements('achievements');
+  const { isLoading: isUserAchievementsLoading, data: UserAchievementsData, isError: isUserAchievementsError, error: UserAchievementsErro, isSuccess: isUserAchievementsSuccess } = useGetUserAchievements('join_table_user_achievements', current_user?.id);
+
+  const progressCircles = document.querySelectorAll('.progress');
+
+  progressCircles.forEach(circle => {
+    const targetProgress = circle.getAttribute('data-target');
+    const percentage = targetProgress / 100;
+    const circumference = 2 * Math.PI * circle.getAttribute('r');
+    const offset = circumference * (1 - percentage);
+    circle.style.strokeDasharray = `${circumference} ${circumference}`;
+    circle.style.strokeDashoffset = `${offset}`;
+  });
+
+  const [isExperienceHovering, setIsExperienceHovering] = useState(false);
+  const [isKarmaHovering, setIsKarmaHovering] = useState(false);
+  const [isAchievementHovering, setIsAchievementHovering] = useState(false);
 
   return (
     <>
       <Cursor />
       <div className='profile'>
         <div className='profile-wrapper'>
+
           <div className='profile-left'>
+
             <div className='profile-left-top'>
-              <img className='profile-picture' src="../src/assets/images/background.jpg" />
-              <div class="profile-info">
-                <h2 class="profile-name">Nom de l'utilisateur</h2>
-                <Button content="Editer le profil" path="/profil/editer"/>
+              <div className="profile-left-top-info">
+                <div className="profile-left-top-info-picture">
+                  <img className='profile-picture' src="../src/assets/images/background.jpg" />
+                </div>
+                <div className="profile-left-top-info-name">
+                  <h2 className="profile-name">{current_user?.username}</h2>
+                  <Button content="Editer le profil" path="/profil/editer"/>
+                </div>
+              </div>
+              <div className='profile-left-top-player-caract'>
+
+              <div className="container">
+
+                <div className="gauge-container" onMouseEnter={() => setIsExperienceHovering(true)} onMouseLeave={() => setIsExperienceHovering(false)}>
+                  <svg className="gauge" viewBox="0 0 150 150">
+                    <circle className="rail" r="67" cx="75" cy="75" />
+                    <circle className="progress" r="67" data-target="10" cx="75" cy="75" />
+                  </svg>
+                  {isExperienceHovering ? (
+                    <span className="center percentage"><span className="value">0/100</span></span>
+                  ) : (
+                    <span className="center percentage"><span className="value">{current_user?.level}</span></span>
+                  )}
+                </div>
+
+                </div>
+
+                <div className="container">
+
+                <div className="gauge-container" onMouseEnter={() => setIsKarmaHovering(true)} onMouseLeave={() => setIsKarmaHovering(false)}>
+                  <svg className="gauge" viewBox="0 0 150 150">
+                    <circle className="rail" r="67" cx="75" cy="75" />
+                    <circle className="progress" r="67" data-target="25" cx="75" cy="75" />
+                  </svg>
+                  {isKarmaHovering ? (
+                    <span className="center percentage"><span className="value">{current_user?.karma} %</span></span>
+                  ) : (
+                    <SiAlchemy className="center icon" />
+                  )}
+                </div>
+
+                </div>
+
+                <div className="container">
+
+                <div className="gauge-container" onMouseEnter={() => setIsAchievementHovering(true)} onMouseLeave={() => setIsAchievementHovering(false)}>
+                  <svg className="gauge" viewBox="0 0 150 150">
+                    <circle className="rail" r="67" cx="75" cy="75" />
+                    <circle className="progress" r="67" data-target="50" cx="75" cy="75" />
+                  </svg>
+                  {isAchievementHovering ? (
+                    <span className="center percentage"><span className="value">{UserAchievementsData?.lenght}/{AchievementsData?.lenght}</span></span>
+                  ) : (
+                    <FaTrophy className="center icon" />
+                  )}
+                </div>
+
+                </div>
+
               </div>
             </div>
+
             <div className='profile-left-bottom'>
               <h2>Caractéristiques</h2>
-              <p>Caract</p>
-              <p>Caract</p>
-              <p>Caract</p>
-              <p>Caract</p>
-              <p>Caract</p>
-              <p>Caract</p>
+              <div className='profile-left-bottom-caract'>
+                <p ><RiSurgicalMaskLine className='discipline' /> Discipline :</p>
+                <p>{current_user?.discipline}</p>
+              </div>
+              <div className='profile-left-bottom-caract'>
+                <p><BsGear className='ingenuity' /> Ingéniosité :</p>
+                <p>{current_user?.ingenuity}</p>
+              </div>
+              <div className='profile-left-bottom-caract'>
+                <p><GiSwordsPower className='willpower' /> Volonté :</p>
+                <p>{current_user?.willpower}</p>
+              </div>
+              <div className='profile-left-bottom-caract'>
+                <p><GiConcentrationOrb className='concentration' /> Concentration :</p>
+                <p>{current_user?.concentration}</p>
+              </div>
+              <div className='profile-left-bottom-caract'>
+                <p><GiChest className='guile' /> Ruse :</p>
+                <p>{current_user?.guile}</p>
+              </div>
+              <div className='profile-left-bottom-caract'>
+                <p><FaFeatherAlt className='dexterity' /> Dextérité :</p>
+                <p>{current_user?.dexterity}</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className='profile-right'>
+            <div className='profile-right-header'>
+              <h2>Historique des parties</h2>
+            </div>
+            <div className='profile-right-cards'>
+              {historiesData && historiesData?.map(history => (
+                <div key={history.id} className='profile-right-card'>
+                  <p>Énigme: {history.enigma_id}</p>
+                  <p>Réalisé le: {history.created_at}</p>
+                </div>
+              ))}
+              {isHistoriesLoading && <p>Loading...</p>}
+              {isHistoriesError && <p>Error: {historiesError.message}</p>}
             </div>
           </div>
-          <div className='profile-right'>
-            <div className='profile-right-header'>Historique des parties</div>
-          </div>
-          <Sidebar />
+
         </div>
       </div>
     </>
