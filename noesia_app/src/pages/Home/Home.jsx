fc29bb3fb50e34903f241fe6e06711337bd286da
Ericcost <eric.costerousse@gmail.com>
2023-03-17 13:22:55 +0100

    Add a new display of the pop-up's story-telling with text-shadow

--- noesia_app/src/pages/Home/Home.jsx
+++ noesia_app/src/pages/Home/Home.jsx
@@ -82,16 +82,18 @@ const Home = () => {
                     <h1>Noesia</h1>
                   </div>
                   <div className='story-plot'>
-                    <p><TypeAnimation
+                    <p>
+                      <TypeAnimation
                       sequence={[
                         1000,
-                        "---------------Au milieu d'un désert aride et désolé, un portail mystérieux venait d'apparaître. Les scientifiques, fascinés par cette étrange apparition, se rassemblèrent autour du portail pour en étudier les propriétés. À chaque nouvelle connexion, le portail multipliait les chemins, offrant de nouvelles perspectives aux chercheurs. Mais la connaissance ne se suffit pas à elle seule pour prospérer. Les intentions de ceux qui l'utilisent peuvent la rendre utile ou dangereuse. Certains voulaient exploiter le portail pour en tirer profit, tandis que d'autres cherchaient à comprendre les secrets de cet univers fantastique." // Types 'One'
+                        "Au milieu d'un désert aride et désolé, un portail mystérieux venait d'apparaître. Les scientifiques, fascinés par cette étrange apparition, se rassemblèrent autour du portail pour en étudier les propriétés. À chaque nouvelle connexion, le portail multipliait les chemins, offrant de nouvelles perspectives aux chercheurs. Mais la connaissance ne se suffit pas à elle seule pour prospérer. Les intentions de ceux qui l'utilisent peuvent la rendre utile ou dangereuse. Certains voulaient exploiter le portail pour en tirer profit, tandis que d'autres cherchaient à comprendre les secrets de cet univers fantastique." // Types 'One'
                       ]}
                       wrapper="strong"
                       cursor={true}
                       repeat={Infinity}
                       style={{ fontSize: '1em' }}
-                      /></p>
+                      />
+                    </p>
                   </div>
                   <div className='story-btn'>
                     <Button onClick={handleStory} content='Skip'/>
