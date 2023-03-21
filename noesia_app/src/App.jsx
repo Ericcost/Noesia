f06b33959dd6ef42c163c0bed74aee320a198650
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-21 10:32:56 +0000

    Add AchievementPopUp Component in App.jsx

--- noesia_app/src/App.jsx
+++ noesia_app/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
 
 //SCSS
 import './styles/main.scss'
@@ -10,17 +11,34 @@ import Register from "./pages/Register/Register";
 import Profile from "./pages/Profile/Profile";
 import Door from "./pages/Door/Door";
 import Map from "./pages/Map/Map";
+import AchievementPopUp from './components/AchievementPopUp/AchievementPopUp';
 
 //Components
 import Footer from "./components/Footer/Footer";
 
 function App() {
+
+  const [achievementUnlocked, setAchievementUnlocked] = useState(false);
+  const [achievementTitle, setAchievementTitle] = useState('');
+
+  const handleUnlockSuccess = () => {
+    setAchievementUnlocked(true);
+  };
+
+  const handleUAchievementTitle = (title) => {
+    setAchievementTitle(title);
+  };
+
   return (
       <div className='App'>
         <main>
+            {achievementUnlocked && <AchievementPopUp 
+              text={achievementTitle} 
+              onClose={() => setAchievementUnlocked(false)} 
+            />}
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path='/porte' element={<Door />} />
+              <Route path='/porte' element={<Door onUnlockSuccess={handleUnlockSuccess} onAchievementTitle={handleUAchievementTitle} />} />
               <Route path="/carte" element={<Map />} />
               <Route path="/connexion" element={<Login />} />
               <Route path="/inscription" element={<Register />} />
