ca903aced4df17cc34565bf427f8416587016ed2
Ericcost <eric.costerousse@gmail.com>
2023-03-21 21:22:56 +0100

    Add a carousel for Enigma2

--- noesia_app/src/pages/Enigmas/Enigma2/Enigma2.jsx
+++ noesia_app/src/pages/Enigmas/Enigma2/Enigma2.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-import './Enigma2.scss'
+import React from 'react';
+
+import Carousel from '../../../components/Carousel/Carousel';
+import CarouselCard from '../../../components/CarouselCard/CarouselCard';
+import CardImg01 from '../../../assets/images/discover01.png';
+import CardImg02 from '../../../assets/images/discover02.png';
+import CardImg03 from '../../../assets/images/discover03.png';
+import CardImg04 from '../../../assets/images/discover04.png';
+
+import './Enigma2.scss';
 
 export default function Enigma2() {
+  const CARDS = 4;
+  const images = [CardImg01, CardImg02, CardImg03, CardImg04];
+
   return (
     <>
       <div className='enigma2'>
@@ -12,14 +23,14 @@ export default function Enigma2() {
           <div className='enigma2-bottom-left'>
             <div className='buttons'>
               Buttons
-            
-
             </div>
           </div>
           <div className='enigma2-bottom-right'>
-              <div className='carousel'>
-                Carousel
-              </div>
+          <Carousel>
+            {[...new Array(CARDS)].map((_, i) => (
+              <CarouselCard img={images[i]}/>
+            ))}
+          </Carousel>
           </div>
         </div>
       </div>
