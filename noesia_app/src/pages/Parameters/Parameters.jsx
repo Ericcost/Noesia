cfc3536ff9c599cdd014e1f8df6bbf2e67ff6acc
Ericcost <eric.costerousse@gmail.com>
2023-03-21 01:29:04 +0100

    New parameters page with 3 toggle

---
+++ noesia_app/src/pages/Parameters/Parameters.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import AvailableSoon from "../../components/AvailableSoon/AvailableSoon";
+
+import './Parameters.scss';
+
+export default function Parameters() {
+  return (
+    <div className='parameters'>
+      <div class="parameters-item">
+        <label>Dark-mode</label>
+        <div className='dark-mode'>
+          <AvailableSoon />
+          <input type="checkbox" value="None" id="dark-mode" name="check" />
+          {/* <label for="dark-mode"></label> */}
+        </div>
+      </div>
+      <div className="parameters-item">
+        <label>Cursor option</label>
+        <div className='cursor-option'>
+          <AvailableSoon />
+          <input type="checkbox" value="None" id="cursor-option" name="check" />
+          {/* <label for="cursor-option"></label> */}
+        </div>
+      </div>
+      <div className="parameters-item">
+        <label>Sound</label>
+        <div className='sound'>
+          <AvailableSoon />
+          <input type="checkbox" value="None" id="sound" name="check" />
+          {/* <label for="sound"></label> */}
+        </div>
+      </div>
+    </div>
+  )
+}
+
