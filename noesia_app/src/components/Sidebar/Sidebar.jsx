6046090f7f9e65fe94405f143f2d5f3095a90387
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-22 17:35:04 +0000

    Remove useless (?) query

--- noesia_app/src/components/Sidebar/Sidebar.jsx
+++ noesia_app/src/components/Sidebar/Sidebar.jsx
@@ -28,7 +28,7 @@ export default function Sidebar() {
   const auth_token = localStorage.getItem('Authorization_token');
   const { isLoading, data, isError, error } = useFetchGet('member-data', 'user', auth_token);
   const current_user = data?.user;
-  const { mutate: deleteUser } = useFetchDelete('users/sign_out', 'user');
+  const { mutate: deleteUser } = useFetchDelete('users/sign_out');
   const logged = auth_token ? true : false;
 
   const pathname = window.location.pathname.includes('/inscription') || window.location.pathname.includes('/connexion') || window.location.pathname.includes(`/profil/${current_user?.id}`);
