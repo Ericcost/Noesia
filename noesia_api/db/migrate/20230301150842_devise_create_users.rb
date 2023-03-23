52d4715f11ebcd0fa904bdb709ac810675b335a0
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-23 09:32:18 +0000

    Add is_admin to user

--- noesia_api/db/migrate/20230301150842_devise_create_users.rb
+++ noesia_api/db/migrate/20230301150842_devise_create_users.rb
@@ -32,6 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
       t.string :username, null: false
+      t.boolean :admin, null: false, default: false
       t.boolean :is_door_passed, null: false, default: false
       
       # Characteristic
