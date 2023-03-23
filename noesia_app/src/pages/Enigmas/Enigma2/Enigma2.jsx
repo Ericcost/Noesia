c407928e7748b649f0601f78009852605358e84a
Ericcost <eric.costerousse@gmail.com>
2023-03-23 11:07:27 +0100

    New enigma2

--- noesia_app/src/pages/Enigmas/Enigma2/Enigma2.jsx
+++ noesia_app/src/pages/Enigmas/Enigma2/Enigma2.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Carousel from '../../../components/Carousel/Carousel';
 import CarouselCard from '../../../components/CarouselCard/CarouselCard';
+import Sun from '../../../components/Sun/Sun';
 import CardImg01 from '../../../assets/images/discover01.webp';
 import CardImg02 from '../../../assets/images/discover02.webp';
 import CardImg03 from '../../../assets/images/discover03.webp';
@@ -11,13 +12,6 @@ import './Enigma2.scss';
 
 export default function Enigma2() {
 
-  function Sun({fireCount}) {
-    const fires = Array.from({length: fireCount}).map((_, index) => (
-      <div key={index} className="sun_fire">
-        <div className="sun_fire_inner"></div>
-      </div>
-  ))};
-
   const CARDS = 4;
   const images = [CardImg01, CardImg02, CardImg03, CardImg04];
 
@@ -25,13 +19,7 @@ export default function Enigma2() {
     <>
       <div className='enigma2'>
         <div className='enigma2-top'>
-          <div className="ui">
-            <div className="sun">
-              {fires}
-              <div className="sun_border"></div>
-            </div>
-            <div className="cover"></div>
-          </div>
+          <Sun fireCount={20} />
         </div>
         <div className='enigma2-bottom'>
           <div className='enigma2-bottom-left'>
@@ -42,7 +30,7 @@ export default function Enigma2() {
           <div className='enigma2-bottom-right'>
           <Carousel>
             {[...new Array(CARDS)].map((_, i) => (
-              <CarouselCard img={images[i]}/>
+              <CarouselCard img={images[i]} key={i}/>
             ))}
           </Carousel>
           </div>
