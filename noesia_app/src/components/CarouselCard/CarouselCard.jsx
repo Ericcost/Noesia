19a619395132d67960d73fffce5660f51e926e92
Ericcost <eric.costerousse@gmail.com>
2023-03-23 14:32:58 +0100

    Rename className of carouselcard

--- noesia_app/src/components/CarouselCard/CarouselCard.jsx
+++ noesia_app/src/components/CarouselCard/CarouselCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function CarouselCard({ img }) {
   return (
-    <div className='card'>
-			<img className='card-background' src={img} />
+    <div className='carousel-card'>
+			<img className='carousel-card-background' src={img} />
     </div>
   )
 }
