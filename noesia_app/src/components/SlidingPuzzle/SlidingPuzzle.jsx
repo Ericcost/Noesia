3d6baa17fd87bf037e8e5a26cc743bba8ff6d14f
Ericcost <eric.costerousse@gmail.com>
2023-03-24 21:49:55 +0100

    Slight responsive modification for carousel, slindinfPuzzle and tile component

--- noesia_app/src/components/SlidingPuzzle/SlidingPuzzle.jsx
+++ noesia_app/src/components/SlidingPuzzle/SlidingPuzzle.jsx
@@ -22,7 +22,7 @@ const SlidingPuzzle = ({ image, gridSize, onUnlockSuccess, onAchievementTitle })
   const { mutate: unlockF11Achievement, isSuccess: unlockF11AchievementSuccess } = useFetchPost(`join_table_user_achievements`);
 
   const TILE_COUNT = gridSize * gridSize;
-  const BOARD_SIZE = 640;
+  const BOARD_SIZE = 480;
   const TILE_SIZE = BOARD_SIZE / gridSize;
 
   const generateSolvedTiles = () => {
@@ -38,7 +38,6 @@ const SlidingPuzzle = ({ image, gridSize, onUnlockSuccess, onAchievementTitle })
   const [tiles, setTiles] = useState([]);
   const [puzzleSolved, setPuzzleSolved] = useState(false);
 
-  // Génère les tuiles initiales
   const generateInitialTiles = () => {
     const initialTiles = Array.from(
       { length: TILE_COUNT - 1 },
@@ -51,7 +50,6 @@ const SlidingPuzzle = ({ image, gridSize, onUnlockSuccess, onAchievementTitle })
     setTiles(shuffle(generateInitialTiles()));
   }, []);
 
-  // Mélange les tuiles
   const shuffle = (array) => {
     let currentIndex = array.length,
       temporaryValue,
