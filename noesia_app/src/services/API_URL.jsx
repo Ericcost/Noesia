bf76f64efd1cb2ad0b2dfd2e85de42037c25ea3c
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-28 14:47:12 +0100

    Modif API_URL

--- noesia_app/src/services/API_URL.jsx
+++ noesia_app/src/services/API_URL.jsx
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:3000";
-export const API_URL ="https://noesia-api.fly.dev"
+export const API_URL = "http://localhost:3000";
+// export const API_URL ="https://noesia-api.fly.dev"
